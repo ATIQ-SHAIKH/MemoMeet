70ef86a20baa897677845566781a12e0ce7da48b
atiq-aib <atiq@aibuddha.in>
2024-12-07 12:30:26 +0530

    :adhesive_bandage: Trying to fix cookie issue

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const helmet = require("helmet");
 const obscureHeader = require("./server/middleware/obscureHeader");
 const {
-    ALLOWED_DOMAIN,
-    SCRIPT_SOURCES,
+  ALLOWED_DOMAIN,
+  SCRIPT_SOURCES,
 } = require("config");
 require('dotenv').config();
 const apiRoutes = require("./server/routes")
@@ -13,66 +13,74 @@ const apiRoutes = require("./server/routes")
 const app = express();
 app.enable("trust proxy");
 app.use(
-    helmet.contentSecurityPolicy({
-        defaultSrc: ["'self'"],
-        scriptSrc: SCRIPT_SOURCES,
-    }),
+  helmet.contentSecurityPolicy({
+    defaultSrc: ["'self'"],
+    scriptSrc: SCRIPT_SOURCES,
+  }),
 );
 app.use(
-    helmet({
-        //contentSecurityPolicy: false,
-        contentSecurityPolicy: {
-            directives: {
-                defaultSrc: ["'self'"], // Allow resources from the same origin
-                scriptSrc: ["'self'", "'unsafe-inline'"], // Allow scripts from the same origin and inline scripts
-                styleSrc: ["'self'", "'unsafe-inline'"], // Allow styles from the same origin and inline styles
-                imgSrc: ["'self'", "data:"], // Allow images from the same origin and data URIs
-                fontSrc: [
-                    "'self'",
-                    "https://fonts.googleapis.com",
-                    "https://fonts.gstatic.com",
-                ], // Example for Google Fonts
-                objectSrc: ["'none'"], // Disallow embedding of objects
-                frameSrc: ["'self'"], // Allow iframes from the same origin
-                //upgradeInsecureRequests: true, // Upgrade HTTP to HTTPS automatically
-                //reportUri: "/csp-violation" // Endpoint to report CSP violations (optional)
-            },
-        },
-        hidePoweredBy: true,
-    }),
+  helmet({
+    //contentSecurityPolicy: false,
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], // Allow resources from the same origin
+        scriptSrc: ["'self'", "'unsafe-inline'"], // Allow scripts from the same origin and inline scripts
+        styleSrc: ["'self'", "'unsafe-inline'"], // Allow styles from the same origin and inline styles
+        imgSrc: ["'self'", "data:"], // Allow images from the same origin and data URIs
+        fontSrc: [
+          "'self'",
+          "https://fonts.googleapis.com",
+          "https://fonts.gstatic.com",
+        ], // Example for Google Fonts
+        objectSrc: ["'none'"], // Disallow embedding of objects
+        frameSrc: ["'self'"], // Allow iframes from the same origin
+        //upgradeInsecureRequests: true, // Upgrade HTTP to HTTPS automatically
+        //reportUri: "/csp-violation" // Endpoint to report CSP violations (optional)
+      },
+    },
+    hidePoweredBy: true,
+  }),
 );
 app.use(obscureHeader);
 app.use(
-    express.json({
-        limit: "2mb",
-    }),
+  express.json({
+    limit: "2mb",
+  }),
 );
 app.use(
-    express.urlencoded({
-        limit: "5mb",
-        extended: true,
-    }),
+  express.urlencoded({
+    limit: "5mb",
+    extended: true,
+  }),
 );
 app.use(
-    cors({
-        origin: ALLOWED_DOMAIN,
-        methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-        credentials: true,
-    }),
+  cors({
+    origin: ALLOWED_DOMAIN,
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    credentials: true,
+  }),
 );
 
 app.get("/example", (req, res) => {
-    console.log(req.ip); // Logs client's IP address
-    res.send(`Client IP: ${req.ip}`);
+  console.log(req.ip); // Logs client's IP address
+  res.send(`Client IP: ${req.ip}`);
 });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://memo-meet.netlify.app/');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 // Load APIs
 app.use("/api", apiRoutes);
 
 // Serve static build files
 app.get("/*", (_, res) =>
-    res.json({ msg: "memomeet APIs" })
-    // res.send(WELCOME_PAGE)
+  res.json({ msg: "memomeet APIs" })
+  // res.send(WELCOME_PAGE)
 );
 
 require("./server/models");
@@ -80,14 +88,14 @@ require("./server/models");
 const { Server } = require("socket.io");
 
 const server = app.listen(process.env.SRV_PORT, () =>
-    console.log(`🚀 Server running at ${process.env.SRV_PORT}`),
+  console.log(`🚀 Server running at ${process.env.SRV_PORT}`),
 );
 const io = new Server(server, {
-    cors: {
-        origin: ALLOWED_DOMAIN,
-        methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-        credentials: true,
-    }
+  cors: {
+    origin: ALLOWED_DOMAIN,
+    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
+    credentials: true,
+  }
 });
 
 // Store participants by meeting code
